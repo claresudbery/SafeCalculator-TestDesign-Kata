7a8aa24f4195b9771cc052f34ae34a70f5f2b491
Clare Sudbery <clareworking@claresudbery.co.uk>
2024-04-29 20:31:15 +0100

    Added failing tests with test doubles using Moq framework

--- csharp/Calculator.Tests/SafeCalculatorNUnitTest.cs
+++ csharp/Calculator.Tests/SafeCalculatorNUnitTest.cs
@@ -2,6 +2,8 @@ using Calculator;
 using NUnit.Framework;
 using Assert = NUnit.Framework.Assert;
 
+using Moq;
+
 namespace TestCalculator;
 
 [TestFixture]
@@ -10,6 +12,27 @@ public class SafeCalculatorNUnitTest
     [Test]
     public void DoNotThrowWhenAuthorized()
     {
-        // TODO: demonstrate the bug in Calculator.Add()
+        // Arrange
+        var authorizer = new Mock<IAuthorizer>();
+        authorizer.Setup(m => m.Authorize()).Returns(true);
+        var safeCalculator = new SafeCalculator(authorizer.Object);
+        
+        // Act
+        var result = safeCalculator.Add(1, 2);
+        
+        // Assert
+        Assert.Equals(3, result);
+    }
+    
+    [Test]
+    public void ThrowWhenNotAuthorized()
+    {
+        // Arrange
+        var authorizer = new Mock<IAuthorizer>();
+        authorizer.Setup(m => m.Authorize()).Returns(false);
+        var safeCalculator = new SafeCalculator(authorizer.Object);
+        
+        // Act & Assert
+        Assert.Throws<Exception>(() => safeCalculator.Add(1, 2));
     }
 }
